45cb9c6676e0974ff34d392e2d328fbbe41937a8
unknown <yinma2448@gmail.com>
2024-06-12 11:29:25 +0800

    project setup & linking

--- opengl_playground/src/main.cpp
+++ opengl_playground/src/main.cpp
@@ -1,7 +1,157 @@
-#include <iostream>
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
 
-int main()
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+
+
+static std::string ParseShader(const std::string& filepath)
 {
-	std::cout << "testing" << std::endl;
-	return 0;
+    std::ifstream stream(filepath);
+    std::string line;
+    std::stringstream ss;
+
+    while (std::getline(stream, line))
+    {
+        ss << line << "\n";
+    }
+
+    return ss.str();
+}
+
+
+static unsigned int ComplieShader(unsigned int type, const std::string& source)
+{
+    unsigned int id = glCreateShader(type);
+    const char* src = source.c_str();
+    glShaderSource(id, 1, &src, nullptr);
+    glCompileShader(id);
+
+
+    int result;
+    glGetShaderiv(id, GL_COMPILE_STATUS, &result);
+    if (result == GL_FALSE)
+    {
+        std::cout << "fail to load " << (type == GL_VERTEX_SHADER ? "vertex" : "fragment") << std::endl;
+        glDeleteShader(id);
+        return 0;
+    }
+
+
+    return id;
+}
+
+static int CreateShader(const std::string& vertexShader, const std::string& fragmentShader)
+{
+    unsigned int program = glCreateProgram();
+    unsigned int vs = ComplieShader(GL_VERTEX_SHADER, vertexShader);
+    unsigned int fs = ComplieShader(GL_FRAGMENT_SHADER, fragmentShader);
+
+    glAttachShader(program, vs);
+    glAttachShader(program, fs);
+    glLinkProgram(program);
+    glValidateProgram(program);
+
+    glDeleteShader(vs);
+    glDeleteShader(fs);
+
+    return program;
+}
+
+int main(void)
+{
+    GLFWwindow* window;
+
+    /* Initialize the library */
+    if (!glfwInit())
+        return -1;
+
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+    //glfwSwapInterval(1);
+
+    if (glewInit() != GLEW_OK)
+    {
+        std::cout << "glew fail to load!" << std::endl;
+        return -1;
+    }
+
+    std::cout << glGetString(GL_VERSION) << std::endl;
+
+    float position[] = {
+        -0.5f, -0.5f,
+        0.5f, -0.5f,
+        0.5f, 0.5f,
+        -0.5f, 0.5f,
+    };
+
+    unsigned int indices[] = {
+        0, 1, 2,
+        2, 3, 0
+    };
+
+    unsigned int buffer;
+    glGenBuffers(1, &buffer);
+    glBindBuffer(GL_ARRAY_BUFFER, buffer);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(float) * 8, position, GL_STATIC_DRAW);
+
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
+
+    unsigned int ibo;
+    glGenBuffers(1, &ibo);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(unsigned int) * 6, indices, GL_STATIC_DRAW);
+
+    std::string vertexShader = ParseShader("./src/vertexShader.txt");
+    std::string fragmentShader = ParseShader("./src/fragmentShader.txt");
+
+    unsigned int shader = CreateShader(vertexShader, fragmentShader);
+    glUseProgram(shader);
+
+    int location = glGetUniformLocation(shader, "u_Color");
+    glUniform4f(location, 0.2f, 0.3f, 0.4f, 1.0f);
+    float rColor = 0.0f;
+    float increment = 0.05f;
+
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window))
+    {
+        /* Render here */
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        //glDrawArrays(GL_TRIANGLES, 0, 3);
+        glUniform4f(location, rColor, 0.3f, 0.4f, 1.0f);
+        if (rColor >= 1.0f)
+        {
+            rColor = 1.0f;
+            increment = -0.05f;
+        }
+        else if (rColor <= 0.0f)
+        {
+            rColor = 0.0f;
+            increment = 0.05f;
+        }
+        rColor += increment;
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
+
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+
+        /* Poll for and process events */
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
 }
