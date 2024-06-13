1a7093b55a2a2b710c03a4b689556de4b03c5faa
unknown <yinma2448@gmail.com>
2024-06-13 10:17:36 +0800

    add texture

--- opengl_playground/src/main.cpp
+++ opengl_playground/src/main.cpp
@@ -6,24 +6,23 @@
 #include "vbo.h"
 #include "ebo.h"
 
+#include "external/stb/stb_image.h"
+
 #include <iostream>
 
 
-// setup vertex
-float vertices[] = {
-    -0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f, // Lower left corner
-    0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f, // Lower right corner
-    0.0f, 0.5f * float(sqrt(3)) * 2 / 3, 0.0f, // Upper corner
-    -0.5f / 2, 0.5f * float(sqrt(3)) / 6, 0.0f, // Inner left
-    0.5f / 2, 0.5f * float(sqrt(3)) / 6, 0.0f, // Inner right
-    0.0f, -0.5f * float(sqrt(3)) / 3, 0.0f // Inner down
+float vertices[] =
+{
+    -0.5f, -0.5f, 0.0f,     1.0f, 0.0f, 0.0f,	0.0f, 0.0f, // Lower left corner
+    -0.5f,  0.5f, 0.0f,     0.0f, 1.0f, 0.0f,	0.0f, 1.0f, // Upper left corner
+     0.5f,  0.5f, 0.0f,     0.0f, 0.0f, 1.0f,	1.0f, 1.0f, // Upper right corner
+     0.5f, -0.5f, 0.0f,     1.0f, 1.0f, 1.0f,	1.0f, 0.0f  // Lower right corner
 };
 
 unsigned int indices[] =
 {
-    0, 3, 5, // Lower left triangle
-    3, 2, 4, // Upper triangle
-    5, 4, 1 // Lower right triangle
+    0, 1, 2, // Upper triangle
+    0, 2, 3 // Lower triangle
 };
 
 
@@ -59,10 +58,43 @@ int main(void)
     VAO vao;
     VBO vbo(vertices, sizeof(vertices));
     EBO ebo(indices, sizeof(indices));
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), NULL);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
     glEnableVertexAttribArray(0);
+    glEnableVertexAttribArray(1);
+    glEnableVertexAttribArray(2);
+
+
     Shader shader("./res/vertexShader.shader", "./res/fragmentShader.shader");
     shader.bind();
+
+    int width, height, channel;
+    stbi_set_flip_vertically_on_load(true);
+    unsigned char* bytes = stbi_load("./res/texture.png", &width, &height, &channel, 0);
+
+    if (!bytes)
+    {
+        std::cout << "img fail to loaded" << std::endl;
+    }
+
+    unsigned int texture;
+    glGenTextures(1, &texture);
+    glActiveTexture(GL_TEXTURE0);
+    glBindTexture(GL_TEXTURE_2D, texture);
+
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, bytes);
+    glGenerateMipmap(GL_TEXTURE_2D);
+    stbi_image_free(bytes);
+    glBindTexture(GL_TEXTURE_2D, texture);
+
+    unsigned int tex0 = glGetUniformLocation(shader.shaderID, "tex0");
+    glUniform1i(tex0, 0);
 
 
     /* Loop until the user closes the window */
@@ -70,7 +102,7 @@ int main(void)
     {
         glClear(GL_COLOR_BUFFER_BIT);
         /* draw call */
-        glDrawElements(GL_TRIANGLES, 9, GL_UNSIGNED_INT, 0);
+        glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, 0);
 
         glfwSwapBuffers(window);
         glfwPollEvents();
