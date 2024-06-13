ab9f6c4fcf4b742dec4544522a931e06467583e6
unknown <yinma2448@gmail.com>
2024-06-13 15:04:38 +0800

    add glm

--- opengl_playground/src/main.cpp
+++ opengl_playground/src/main.cpp
@@ -1,5 +1,9 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
+#include<glm/glm.hpp>
+#include<glm/gtc/matrix_transform.hpp>
+#include<glm/gtc/type_ptr.hpp>
+
 
 #include "shader.h"
 #include "vao.h"
@@ -48,7 +52,6 @@ int main(void)
         glfwTerminate();
         return -1;
     }
-
     glfwMakeContextCurrent(window);
     //glfwSwapInterval(1);
 
@@ -64,6 +67,11 @@ int main(void)
     VAO vao;
     VBO vbo(vertices, sizeof(vertices));
     EBO ebo(indices, sizeof(indices));
+    vao.bind();
+    vbo.bind();
+    ebo.bind();
+
+    // layout
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
     glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
     glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
@@ -71,18 +79,39 @@ int main(void)
     glEnableVertexAttribArray(1);
     glEnableVertexAttribArray(2);
 
-
     Shader shader("./res/vertexShader.shader", "./res/fragmentShader.shader");
     shader.bind();
 
     Texture texture("./res/brick.png", GL_TEXTURE0, 0);
+    texture.bind();
+
     shader.setUniform1i("tex0", texture.unitID);
 
+    glm::mat4 model = glm::mat4(1.0f);
+    glm::mat4 view = glm::mat4(1.0f);
+    glm::mat4 proj = glm::mat4(1.0f);
+
+    model = glm::rotate(model, glm::radians(45.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+    view = glm::translate(view, glm::vec3(0.0, -0.25f, -10.0f));
+    proj = glm::perspective(glm::radians(45.0f), 1.0f, 0.1f, 100.0f);
+
+    int modelLoc = glGetUniformLocation(shader.shaderID, "model");
+    int viewLoc = glGetUniformLocation(shader.shaderID, "view");
+    int projLoc = glGetUniformLocation(shader.shaderID, "proj");
+
+    glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+    glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
+    glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(proj));
+
+    glEnable(GL_DEPTH_TEST);
 
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
-        glClear(GL_COLOR_BUFFER_BIT);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        model = glm::rotate(model, glm::radians(1.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+        glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
         /* draw call */
         glDrawElements(GL_TRIANGLES, 3 * 6, GL_UNSIGNED_INT, 0);
 
