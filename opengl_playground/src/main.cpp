dd204feae3ce64eed10c72eb55d71ca7bb2455b8
unknown <yinma2448@gmail.com>
2024-06-17 08:45:58 +0800

    rename shaders

--- opengl_playground/src/main.cpp
+++ opengl_playground/src/main.cpp
@@ -52,52 +52,6 @@ unsigned int indices[] =
 };
 
 
-float lightVertices[] =
-{ //     COORDINATES     //
-    -0.1f, -0.1f,  0.1f,
-    -0.1f, -0.1f, -0.1f,
-     0.1f, -0.1f, -0.1f,
-     0.1f, -0.1f,  0.1f,
-    -0.1f,  0.1f,  0.1f,
-    -0.1f,  0.1f, -0.1f,
-     0.1f,  0.1f, -0.1f,
-     0.1f,  0.1f,  0.1f
-};
-
-
-unsigned int lightIndices[] =
-{
-    0, 1, 2,
-    0, 2, 3,
-    0, 4, 7,
-    0, 7, 3,
-    3, 7, 6,
-    3, 6, 2,
-    2, 6, 5,
-    2, 5, 1,
-    1, 5, 4,
-    1, 4, 0,
-    4, 5, 6,
-    4, 6, 7
-};
-
-
-float planeVertices[] =
-{ //     COORDINATES     // normal
-    -4.0f, 0.0f, -4.0f, 0.0f, 1.0f, 0.0f,
-    -4.0f, 0.0f, 4.0f, 0.0f, 1.0f, 0.0f,
-     4.0f, 0.0f, -4.0f, 0.0f, 1.0f, 0.0f,
-     4.0f, 0.0f, 4.0f, 0.0f, 1.0f, 0.0f,
-};
-
-
-unsigned int planeIndices[] =
-{
-    0, 1, 2,
-    2, 1, 3,
-};
-
-
 int main(void)
 {
     GLFWwindow* window;
@@ -154,7 +108,7 @@ int main(void)
     pritimitVbo.setLayoutf(3, 3, 11, 8);
 
     // init shader
-    Shader pirimitShader("./res/vertexShader.shader", "./res/fragmentShader.shader");
+    Shader pirimitShader("./res/simple.vs", "./res/simple.fs");
     pirimitShader.bind();
     Texture brickTexture("./res/brick.png", GL_TEXTURE0, 0);
     brickTexture.bind();
@@ -169,61 +123,6 @@ int main(void)
     pirimitShader.unbind();
     pirimitVao.unbind();
 
-    /* init plane */
-    VAO planeVAO;
-    VBO planeVBO(planeVertices, sizeof(planeVertices));
-    EBO planeEBO(planeIndices, sizeof(planeIndices));
-    planeVAO.bind();
-    planeVBO.bind();
-    planeEBO.bind();
-
-    planeVBO.setLayoutf(0, 3, 6, 0);
-    planeVBO.setLayoutf(1, 3, 6, 3);
-
-    Shader planeShader("./res/planeVertex.shader", "./res/planeFragment.shader");
-    planeShader.bind();
-
-    glm::mat4 planeModel = glm::mat4(1.0f);
-    planeShader.setUniformMatrix4fv("model", glm::value_ptr(planeModel));
-    planeShader.setUniformMatrix4fv("cameraMatrix", glm::value_ptr(cameraMat));
-    planeShader.setUniform3fv("cameraPosition", glm::value_ptr(camera.position));
- 
-    /* init light source */
-    VAO lightVAO;
-    VBO lightVBO(lightVertices, sizeof(lightVertices));
-    EBO lightEBO(lightIndices, sizeof(lightIndices));
-    lightVAO.bind();
-    lightVBO.bind();
-    lightEBO.bind();
-
-    lightVBO.setLayoutf(0, 3, 3, 0);
-
-    Shader lightShader("./res/lightVertex.shader", "./res/lightFragment.shader");
-    lightShader.bind();
-
-    glm::vec4 lightColor(0.9f, 0.9f, 0.9f, 1.0f);
-    glm::vec3 lightPosition(0.8f, 0.8f, 0.8f);
-    glm::mat4 lightModel = glm::translate(lightPosition);
-
-    lightShader.setUniformMatrix4fv("model", glm::value_ptr(lightModel));
-    lightShader.setUniformMatrix4fv("cameraMatrix", glm::value_ptr(cameraMat));
-    lightShader.setUniform4fv("lightColor", glm::value_ptr(lightColor));
-
-    lightShader.unbind();
-    lightVAO.unbind();
-
-    /* init shaders uniform */
-    pirimitShader.bind();
-    pirimitShader.setUniform3fv("lightPos", glm::value_ptr(lightPosition));
-    pirimitShader.setUniform4fv("lightColor", glm::value_ptr(lightColor));
-    pirimitShader.setUniform3fv("camPos", glm::value_ptr(camera.position));
-    pirimitShader.unbind();
-
-    planeShader.bind();
-    planeShader.setUniform3fv("lightPosition", glm::value_ptr(lightPosition));
-    planeShader.setUniform4fv("lightColor", glm::value_ptr(lightColor));
-    planeShader.unbind();
-
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
@@ -236,30 +135,11 @@ int main(void)
 
         /* draw call */
 
-        // draw light
-        glm::vec3 lPos = glm::rotate(lightPosition, glm::radians((float)glfwGetTime()*50.0f), glm::vec3(0.0f, 1.0f, 0.0f));
-        lightVAO.bind();
-        lightShader.bind();
-        lightModel = glm::translate(lPos);
-        lightShader.setUniformMatrix4fv("model", glm::value_ptr(lightModel));
-        lightShader.setUniformMatrix4fv("cameraMatrix", glm::value_ptr(cameraMat));
-        glDrawElements(GL_TRIANGLES, 3 * 12, GL_UNSIGNED_INT, 0);
-
         // draw pirimit
         pirimitVao.bind();
         pirimitShader.bind();
-        pirimitShader.setUniform3fv("camPos", glm::value_ptr(camera.position));
-        pirimitShader.setUniform3fv("lightPos", glm::value_ptr(lPos));
         pirimitShader.setUniformMatrix4fv("cameraMat", glm::value_ptr(cameraMat));
         glDrawElements(GL_TRIANGLES, 3 * 6, GL_UNSIGNED_INT, 0);
-
-        // draw plane
-        planeVAO.bind();
-        planeShader.bind();
-        planeShader.setUniform3fv("cameraPosition", glm::value_ptr(camera.position));
-        planeShader.setUniform3fv("lightPosition", glm::value_ptr(lPos));
-        planeShader.setUniformMatrix4fv("cameraMatrix", glm::value_ptr(cameraMat));
-        glDrawElements(GL_TRIANGLES, 3 * 2, GL_UNSIGNED_INT, 0);
 
         glfwSwapBuffers(window);
         glfwPollEvents();
